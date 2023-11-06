f21ba544156c1bcbc584403364856a862bcd22cb
zhaoying-zoe <1130051785@qq.com>
2023-11-06 19:14:37 +0800

    refresh中转页

---
+++ 05 - vue/vue2/src/components/refresh/refresh.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>123456</div>
+</template>
+
+<script>
+    export default {
+        name: 'refreshPage',
+        beforeRouteEnter(to, from, next) {
+            // 路由跳转前
+            const name = to.params.name || from.name;
+
+            next(vm => {
+                vm.$router.push({
+                    name,
+                    params: to.params
+                })
+            });
+        }
+    }
+</script>
